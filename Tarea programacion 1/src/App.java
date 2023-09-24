78d145871fba269f40415017f9333c6f13bae0a9
DESKTOP-PVKV73J\javin <javint321@gmail.com>
2023-09-24 11:54:50 +0200

    third commit

--- Tarea programacion 1/src/App.java
+++ Tarea programacion 1/src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hola, mundo!");
         System.out.println("voy a aprobar!");
+        System.out.println("la asignatura de Pablo!");
     }
 }
